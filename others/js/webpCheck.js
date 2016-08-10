d5ab7d6e145c9e7ae1391273ca39cc6ea5e30b19
zhanghexuan <136789658@qq.com>
2016-08-10 12:03:34 +0800

    webp检测

--- others/js/webpCheck.js
+++ others/js/webpCheck.js
@@ -34,3 +34,11 @@
     webpTest(webpSrc, 'webp');
     webpTest(webpanimationSrc, 'webpanimation');
 })();
+
+(function() {
+    var WebP = new Image();
+    WebP.onload = WebP.onerror = function() {
+        window.supportWebP = WebP.height == 1;
+    };
+    WebP.src = "data:image/webp;base64,UklGRiYAAABXRUJQVlA4IBoAAAAwAQCdASoBAAEAAAAMJaQAA3AA/v89WAAAAA==";
+})();
