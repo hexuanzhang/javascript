f8326463f844a78f2b055cb2c1dfb33985c28298
hexuanzhang <136789658@qq.com>
2017-12-18 23:52:46 +0800

    random
    生成随机范围内的整数

---
+++ others/random/random.ts
@@ -0,0 +1,23 @@
+// 随机生成 (min, max) 之间的整数
+const random = () => {
+    // Math.floor(Math.random()*(max - min - 1) + min + 1)
+    return Math.ceil(Math.random()*(max - min - 1) + min);
+};
+
+// 随机生成 [min, max) 之间的整数
+const random = () => {
+    // Math.floor(Math.random()*(max - min) + min)
+    return Math.ceil(Math.random()*(max - min) + min - 1);
+};
+
+// 随机生成 (min, max] 之间的整数
+const random = () => {
+    // Math.floor(Math.random()*(max - min) + min + 1)
+    return Math.ceil(Math.random()*(max - min) + min);
+};
+
+// 随机生成 (min, max] 之间的整数
+const random = () => {
+    // Math.floor(Math.random()*(max - min + 1) + min)
+    return Math.ceil(Math.random()*(max - min + 1) + min - 1);
+};
