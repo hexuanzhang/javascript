2dd7d314bebdb59c4af956214bc044a47b060a0c
hexuanzhang <136789658@qq.com>
2017-12-19 00:12:20 +0800

    random
    补充完善

--- others/random/random.ts
+++ others/random/random.ts
@@ -7,6 +7,7 @@ const random = () => {
 // 随机生成 [min, max) 之间的整数
 const random = () => {
     // Math.floor(Math.random()*(max - min) + min)
+    // Number.parseInt(Math.random()*(max - min) + min, 10)
     return Math.ceil(Math.random()*(max - min) + min - 1);
 };
 
@@ -16,8 +17,9 @@ const random = () => {
     return Math.ceil(Math.random()*(max - min) + min);
 };
 
-// 随机生成 (min, max] 之间的整数
+// 随机生成 [min, max] 之间的整数
 const random = () => {
     // Math.floor(Math.random()*(max - min + 1) + min)
+    // Math.round(Math.random()*(max - min) + min)
     return Math.ceil(Math.random()*(max - min + 1) + min - 1);
 };
